a563ed09b729b45e43c1a53e382bc9f69c182281
unknown <richardjamesgaspar23@gmail.com>
2025-04-08 09:41:05 +0800

    update 04082025

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const port = 8000;
 const cors = require('cors');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: '',
-	port:'',
-    user: '',
-    password: '',
-    database: ''
+    host: '192.168.250.10',
+	port:'3307',
+    user: 'root',
+    password: 'R00t',
+    database: 'hospital_dbo'
   })
   
     app.use(cors());
@@ -22,10 +22,12 @@ const connection = mysql.createConnection({
 app.listen(port, ()=> console.log("server is online"));
 
 app.get("/MGH", (req, res)=>{
-	
-	var query = `select CONCAT(hperson.patlast,", ",hperson.patfirst," ",hperson.patmiddle) AS Fullname, DATE_FORMAT(hdocord.dodtepost, '%Y-%m-%d') as dateMGH from hdocord inner join 
+
+  var query = `select hdocord.hpercode as healthno, CONCAT(hperson.patlast,", ",hperson.patfirst," ",IFNULL(hperson.patmiddle, "N/A")) AS Fullname, DATE_FORMAT(hdocord.dodtepost, '%Y-%m-%d') as dateMGH from hdocord inner join 
 	             hperson on hperson.hpercode = hdocord.hpercode 
 	             where orcode = 'DISCH'`;
+	
+	
 	
 	connection.query(query, (err, results)=>{
 		
@@ -37,6 +39,30 @@ app.get("/MGH", (req, res)=>{
       }
 	  
 	  res.send({mghData:results})
+		
+	});
+	
+});
+
+
+app.get("/dispo", (req, res)=>{
+
+  var query = `select hperson.hpercode as healthno, concat(hperson.patlast,", ", hperson.patfirst," ", hperson.patmiddle) as fullname, DATE_FORMAT(hadmlog.disdate, '%Y-%m-%d') as DateDisposition, hadmlog.dispcode from hadmlog inner join 
+  hperson on hperson.hpercode = hadmlog.hpercode 
+  where hadmlog.dispcode = 'DIEDD' or hadmlog.dispcode = 'DAMA'`;
+	
+	
+	
+	connection.query(query, (err, results)=>{
+		
+		console.log(results);
+		
+		if (err) {
+        console.error('error running query:', err);
+        return;
+      }
+	  
+	  res.send({disData:results})
 		
 	});
 	
